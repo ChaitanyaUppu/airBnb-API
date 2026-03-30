d1831232e13d4020b04c3a505577d8613f3a8389
ChaitanyaUppu <chaitanyasai327@gmail.com>
2026-03-30 20:11:59 +0530

    get updated and put request

--- routes/hotel.js
+++ routes/hotel.js
@@ -2,11 +2,12 @@ const express = require('express');
 const auth = require('../middleware/auth');
 const Hotel = require('../models/hotel');
 const ContactInfo = require('../models/contactInfo');
+const contactInfo = require('../models/contactInfo');
 
-const router = express.Router(); 
+const router = express.Router();
 
 router.get('/', auth, auth.isAdmin, async (req, res) => {
-  const hotels = await Hotel.find({ userId: req.user.id });
+  const hotels = await Hotel.find({ userId: req.user.id }).populate('contactInfo');
   res.json(hotels);
 });
 
@@ -14,7 +15,7 @@ router.get('/:hotelId', auth, auth.isAdmin, async (req, res) => {
   const hotel = await Hotel.findOne({
     _id: req.params.hotelId,
     userId: req.user.id
-  });
+  }).populate('contactInfo');
 
   if (!hotel) return res.status(404).json({ msg: 'hotel not found' });
   res.json(hotel);
@@ -27,11 +28,31 @@ router.post('/', auth, auth.isAdmin, async (req, res) => {
   const hotel = new Hotel({
     ...req.body,
     contactInfo: contactinfo._id,
-    userId: req.user.id 
+    userId: req.user.id
   });
 
   await hotel.save();
   res.json(hotel);
 });
+
+router.put('/:hotelId', auth, auth.isAdmin, async (req, res) => {
+  try {
+    
+    const hotel = await Hotel.findOne({ _id: req.params.hotelId, userId: req.user.id });
+    if (!hotel) return res.status(404).json({ msg: 'hotel not found' });
+    // as we save contactInfo as id in mongo db we must seprate that contact info 
+    // and then save it and update both
+    const {contactInfo,...hotelData}=req.body;
+    Object.assign(hotel,hotelData);
+    if(contactInfo&&hotel.contactInfo){
+      await ContactInfo.findByIdAndUpdate(hotel.contactInfo,contactInfo,{returnDocument:'after',runValidators:true});
+    }
+    await hotel.save();
+    res.json(hotel);
+  } catch (err) {
+    res.status(500).json({ msg: 'server erroe' });
+  }
+
+})
 
 module.exports = router;
